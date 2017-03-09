f36c2350a2ac0131460bce67efc1518c58946abc
Zack Fullerton <zachary.e.fullerton@gmail.com>
2017-03-08 20:56:23 -0600

    Building out pouchDB structure

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var PouchDb = require 'pouchdb';
+var PouchDb = require ('pouchdb');
 
 var db = new PouchDb('rystel');
 
@@ -7,30 +7,60 @@ db.info().then(function (info) {
 });
 
 var doc = {
-	"_id": ,
-	"hero": ,
-	"hero_image": ,
-	"quote": ,
+	"_id": "001",
+	"hero": "",
+	"hero_image": "",
+	"quote": "",
 	"goals": [
       {
-      	"goal_id": ,
+      	"goal_id": "",
       	"goal_tasks": [],
-      	"completed": 
+      	"completed": ""
       }
 	],
 	"roadblocks": [
 	  {
-        "rb_id": ,
-        "rb_text": ,
-        "resolved": 
+        "rb_id": "",
+        "rb_text": "",
+        "resolved": ""
       }  
 	],
 	"mindful": [
       {
-      	"mind_id": ,
-      	"mind_text": 
+      	"mind_id": "",
+      	"mind_text": ""
       }
 	],
 };
+
+db.put(doc, function (err, response) {
+	if (err) {
+      console.log(err);
+	} else {
+	  console.log('Successfully created document')		
+	}
+});
+
+db.info().then(function (info) {
+	console.log(info);
+});
+
+db.get('001', function(err, doc) {
+   if (err) {
+      return console.log(err);
+   } else {
+      console.log(doc);
+   }
+});
+
+// db.destroy(function (err, response) {
+// 	if (err) {
+//       console.log(err);
+// 	} else {
+// 	  console.log('Database deleted')		
+// 	}
+// });
+
+
 
 module.exports = db;
